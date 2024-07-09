b42b7d04869c647a5f6d0e54fa2d8d7397775680
Jose Santos <josemrsantos@gmail.com>
2024-07-09 19:14:39 +0100

    fix sanity_checks_run.py demo script

--- sanity_checks_run.py
+++ sanity_checks_run.py
@@ -2,11 +2,14 @@ import sanity_checks
 import connector_sqlite
 
 
+# This is just a demo usage example for a table in a SQLite database
+# This is the default DB that comes with SQLite, but I have changed a value to demo
+# the functionality of the SanityCheck class
 def main():  # This still has a bug. Will fix later.
-    db_path = "./demo/my_database.sqlite"  # Adjust the path as necessary
-    tests_path = "./tests/sql_tests"  # Adjust the path as necessary
+    db_path = "./demo/Chinook.db"  # Adjust the path as necessary
+    tests_path = "./sql_tests/"  # Adjust the path as necessary
     # Instantiate the SQLite connector
-    db_connector = connector_sqlite.SQLiteConnector(db_path)
+    db_connector = connector_sqlite.SQLiteDB(db_path)
     # Run the SanityCheck
     sanity_checks.SanityCheck(tests_path=tests_path, connector=db_connector)
 
